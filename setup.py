219e6ff4aadb863a8c159799544a4f8ba31b6469
ll@qq.com <ll@qq.com>
2021-05-17 06:19:43 +0800

    ok

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name="pyefun", version="1.0", description="pyefun 为python提供强大且易用的中文函数库，完整的封装了易语言核心支持库所有功能，以及易语言中简单易用的函数",
+      author="duolabmeng(1715109585@qq.com)",
+      py_modules=['core', 'coreUtil'])

fc23298f9d19c295caf0df2a74c65591b782d7cf
ll <ll@163.com>
2021-05-19 14:33:22 +0800

    ok

---
+++ docs/build/html/_sources/modules.rst.txt
@@ -0,0 +1,7 @@
+pyefun
+======
+
+.. toctree::
+   :maxdepth: 4
+
+   pyefun
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,13 @@ release = 'latest'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser'
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.

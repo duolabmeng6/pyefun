d3c5097532c7fbd52d9b3afe18d8e0d305185877
ll <ll@163.com>
2021-05-19 11:38:30 +0800

    ok

---
+++ pyefun/coreUtil/__init__.py
@@ -0,0 +1,5 @@
+from coreUtil import cacheUtil
+from coreUtil import commonlyUtil
+from coreUtil import progiterUtil
+from coreUtil import stringUtil
+from coreUtil import timeUtil
--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
 from distutils.core import setup
 
-setup(name="pyefun", version="1.0", description="pyefun 为python提供强大且易用的中文函数库，完整的封装了易语言核心支持库所有功能，以及易语言中简单易用的函数",
-      author="duolabmeng(1715109585@qq.com)",
-      py_modules=['core', 'coreUtil'])
+# setup(name="pyefun", version="1.0", description="pyefun 为python提供强大且易用的中文函数库，完整的封装了易语言核心支持库所有功能，以及易语言中简单易用的函数",
+#       author="duolabmeng(1715109585@qq.com)",
+#       py_modules=['core', 'coreUtil'])
+
+
+from setuptools import setup, find_packages
+setup(
+      name = "pyefun",
+      version = "1.0",
+      packages=find_packages('.'),
+      # scripts = ['say_hello.py'],
+      # Project uses reStructuredText, so ensure that the docutils get
+      # installed or upgraded on the target machine
+      # install_requires = ['docutils>=0.3'],
+      package_data = {
+            # '': ['*.txt', '*.rst'],
+            # 'core': ['*.py'],
+      },
+      # metadata for upload to PyPI
+      author = "duolabmeng",
+      author_email = "1715109585@qq.com",
+      description = "pyefun 为python提供强大且易用的中文函数库，完整的封装了易语言核心支持库所有功能，以及易语言中简单易用的函数",
+      license = "Apache 2",
+      keywords = "pyefun 易语言",
+      url = "https://github.com/duolabmeng6/pyefun",   # project home page, if any
+)

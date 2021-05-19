236695b23a28e18061355b0fb0c6996dea43dc65
ll <ll@163.com>
2021-05-19 12:58:56 +0800

    ok

--- setup.py
+++ setup.py
@@ -4,11 +4,26 @@ from distutils.core import setup
 #       author="duolabmeng(1715109585@qq.com)",
 #       py_modules=['core', 'coreUtil'])
 
+def parse_description():
+      """
+      Parse the description in the README file
+      CommandLine:
+          pandoc --from=markdown --to=rst --output=README.rst README.md
+          python -c "import setup; print(setup.parse_description())"
+      """
+      from os.path import dirname, join, exists
+      readme_fpath = join(dirname(__file__), 'README.rst')
+      # This breaks on pip install, so check that it exists.
+      if exists(readme_fpath):
+            with open(readme_fpath, 'r',encoding="utf-8") as f:
+                  text = f.read()
+            return text
+      return ''
 
 from setuptools import setup, find_packages
 setup(
       name = "pyefun",
-      version = "1.0.0",
+      version = "1.0.6",
       packages=find_packages('.'),
       # scripts = ['say_hello.py'],
       # Project uses reStructuredText, so ensure that the docutils get
@@ -24,5 +39,7 @@ setup(
       license = "Apache 2",
       keywords = "pyefun 易语言",
       url = "https://github.com/duolabmeng6/pyefun",   # project home page, if any
-      long_description = "README.rst"
+
+      long_description=parse_description(),
+      long_description_content_type='text/markdown',
 )

8cdcc4d07fef1826b63d487c4435f2647b262fac
ll <ll@163.com>
2021-05-20 11:56:33 +0800

    网页访问命令

---
+++ pyefun/networkUtil_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from .networkUtil import *
+
+
+class TestNetworkUtil(unittest.TestCase):
+
+    def test_1(self):
+        data = 网页_取外网IP()
+        print(data)
+
+    def test_2(self):
+        data = 网页_访问("https://www.125.la/")
+        print(data.字节集.decode('utf-8'))
+
+    def test_3(self):
+        data = 网页_访问("https://www.baidu.com/")
+        print(data.字节集.decode('utf-8'))

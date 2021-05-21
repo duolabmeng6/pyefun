dc46c5e5f1b300f8554586c89b81d5d48d7bc789
ll <ll@163.com>
2021-05-21 19:45:04 +0800

    ok

--- pyefun/public.py
+++ pyefun/public.py
@@ -1,8 +1,17 @@
 # -*- coding:utf-8 -*-
 import traceback, datetime
 
-DEBUG = True
-# DEBUG = False
+异常显示信息 = True
+
+
+def 设置_异常处理_显示信息(显示信息=2):
+    """
+
+    :param 显示信息: 0 不显示 1显示简单的 2显示详细的
+    :return:
+    """
+    global 异常显示信息
+    异常显示信息 = 显示信息
 
 
 def 异常处理返回类型逻辑型(function):
@@ -12,12 +21,18 @@ def 异常处理返回类型逻辑型(function):
         try:
             return function(*args, **kwargs)
         except:
-            if (DEBUG):
+            if 异常显示信息 == 2:
                 print("函数异常 %s 时间 %s \r\n%s" % (
                     function.__name__,
                     str(datetime.datetime.now()),
                     traceback.format_exc()
                 ))
+            if 异常显示信息 == 1:
+                print("函数异常 %s 时间 %s" % (
+                    function.__name__,
+                    str(datetime.datetime.now()),
+                ))
+
             return False
 
     return box

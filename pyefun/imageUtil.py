20d3c29bf4200cd90a2340ff4aef1c0d1906ceec
ll <ll@163.com>
2021-06-01 20:57:11 +0800

    图像处理pillow

---
+++ pyefun/imageUtil.py
@@ -0,0 +1,28 @@
+"""
+图像处理
+
+pip install pillow
+
+"""
+from PIL import Image
+import io
+
+
+def image到字节集(image对象):
+    字节流 = io.BytesIO()
+    image对象.save(字节流, format='PNG')
+    图片字节集 = 字节流.getvalue()
+    字节流.close()
+    return 图片字节集
+
+
+def image从字节集加载(图片字节集) -> Image:
+    return Image.open(io.BytesIO(图片字节集))
+
+
+def image取图片宽度高度(image对象: Image.Image):
+    width, height = image对象.size
+    return width, height
+
+def image显示图片(image对象):
+    image对象.show()
---
+++ pyefun/imageUtil_test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from .imageUtil import *
+from .__init__ import *
+from PIL import Image
+
+
+class TestImageUtil(unittest.TestCase):
+
+    def test_1(self):
+        pass
+        文件路径 = 路径_合并(取运行目录(), r"example\wxefun\2.png")
+        # print(文件路径)
+        字节集 = 读入文件(文件路径)
+        print(len(字节集))
+
+        im = image从字节集加载(字节集)
+        # print(im)
+        # im.show()
+        字节集 = image到字节集(im)
+        print(len(字节集))
+
+
+        im = image从字节集加载(字节集)
+        # print(im)
+        # im.show()
+        字节集 = image到字节集(im)
+        print(len(字节集))
+
+        print(image取图片宽度高度(im))
+
+        image显示图片(im)

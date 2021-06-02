a63292819ee8dc230919a0c273b0c66c0f6a84d5
ll <ll@163.com>
2021-06-03 01:32:58 +0800

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ extensions = [
     'recommonmark',
     'sphinx_markdown_tables'
 ]
-autodoc_mock_imports = ['wx', 'gevent','win32api','win32gui']
+autodoc_mock_imports = ['wx', 'gevent','win32api','win32gui','qrcode','pyzbar']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

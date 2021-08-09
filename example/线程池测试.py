3fa287ba7cf7de25e42dc74490cbdfb33927b2d4
ll <ll@163.com>
2021-08-10 06:28:51 +0800

    线程池例子更新

--- example/线程池测试.py
+++ example/线程池测试.py
@@ -2,22 +2,42 @@ from pyefun.processPoolUtil import *
 from pyefun import *
 
 
-def 任务函数(data):
-    # print("data:", data)
-    延时(2)
-    raise Exception("a")
-    return "返回值 %s" % data
+def 任务函数(参数1, 参数2, 参数3):
+    延时(1)
+    print("参数1 {} 参数2 {} 参数3 {}".format(参数1, 参数2, 参数3))
+    return "返回值 {}".format(参数1)
 
 
-def 任务完成(data):
+def 任务完成(future):
     pass
-    # print("任务完成")
+    返回结果 = future.result()
+    print("任务完成 {}".format(返回结果))
+
+
+def 模式1_全部任务一次性投递():
+    print("==========模式1_全部任务一次性投递==========")
+    pool = 线程池(3)
+    for i in range(9):
+        print("======投递任务 {}".format(i))
+        task = pool.投递任务(任务函数, i, "b", "c")
+        pool.设置任务结束回调函数(task, 任务完成)
+    pool.等待()
+    print("========================================")
+
+
+def 模式2_任务完成后投递():
+    print("==========模式2_任务完成后投递==========")
+    pool = 线程池(3, 投递任务时阻塞=True)
+    for i in range(9):
+        print("======投递任务 {}".format(i))
+        task = pool.投递任务(任务函数, i, "b", "c")
+        pool.设置任务结束回调函数(task, 任务完成)
+    pool.等待()
+    print("========================================")
 
 
 if __name__ == "__main__":
-    pool = 线程池(4)
-    tasks = []
-    for i in range(100):
-        print(i)
-        task = pool.投递任务(任务函数, ("%s" % i))
-        pool.设置任务结束回调函数(task, 任务完成)
+
+    模式1_全部任务一次性投递()
+
+    模式2_任务完成后投递()

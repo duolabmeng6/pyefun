8479cc55874d7710b116170c47961209e5c126f9
duolabmeng6 <1715109585@qq.com>
2022-08-14 19:53:52 +0800

    优化代码

---
+++ pyefun/wxefun/__init__.py
@@ -0,0 +1,14 @@
+print("""
+
+wxefun 模块已经移除
+代码在 https://github.com/duolabmeng6/wxefun 中请自行下载引入
+
+移除原因: 我认为qt可以完全替代wx, 如果你觉得wx更好用, 可以自行下载wxefun模块 
+或者 想兼容以前的代码请下载旧版本的 pyefun 
+pip install pyefun==1.2.4.20220713.1607
+该版本就含有wxefun 以后pyefun最新的版本都不会有wxefun
+
+目前推荐使用qt技术本人在积极开发维护,  
+qt设计器 https://github.com/duolabmeng6/QtEasyDesigner
+qt汉化组件 qtefun https://github.com/duolabmeng6/qtefun
+""")

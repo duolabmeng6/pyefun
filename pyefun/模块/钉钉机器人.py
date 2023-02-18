defdc818a481ec9ab97535dc2fa9c65aacfa67a3
“1715109585” <1715109585@qq.com>
2023-02-19 04:26:18 +0800

    钉钉机器人模块

---
+++ pyefun/模块/钉钉机器人.py
@@ -0,0 +1,42 @@
+# 用于操作钉钉机器人的模块
+# 需要安装模块pip install DingtalkChatbot
+# 使用教程 https://github.com/zhuifengshen/DingtalkChatbot
+from dingtalkchatbot.chatbot import DingtalkChatbot, ActionCard, CardItem
+
+
+
+class 钉钉机器人:
+    def __init__(self, webhook, secret=None):
+        self.机器人 = None
+        if secret is None:
+            self.机器人 = DingtalkChatbot(webhook)  # 方式一：通常初始化方式
+        else:
+            self.机器人 = DingtalkChatbot(webhook, secret=secret)  # 方式二：勾选“加签”选项时使用（v1.5以上新功能）
+
+    def 发送文本消息(self, 消息内容):
+        self.机器人.send_text(msg=消息内容)
+
+    def 发送链接消息(self, 消息内容, 消息标题, 消息URL, 消息图片URL):
+        # # Link消息
+        self.机器人.send_link(title=消息标题, text=消息内容, message_url=消息URL, pic_url=消息图片URL)
+
+
+    def 发送markdown消息(self, 消息内容):
+        self.机器人.send_markdown(title="markdown", text=消息内容)
+
+    def 发送图片消息(self, 图片网络地址):
+        self.机器人.send_image(图片网络地址)
+
+    def 整体跳转消息类型(self, 消息标题, 消息内容, 消息URL):
+        按钮组 = [CardItem(title="查看详情", url=消息URL)]
+        卡片 = ActionCard(title=消息标题,
+                                    text=消息内容,
+                                    btns=按钮组,
+                                    btn_orientation=1,
+                                    hide_avatar=1)
+        self.机器人.send_action_card(卡片)
+
+
+
+
+
---
+++ pyefun/模块/钉钉机器人_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from .钉钉机器人 import *
+from pyefun import *
+
+
+class Test_钉钉机器人(unittest.TestCase):
+
+    def test_1(self):
+        环境变量_从文本中加载至系统(读入文本(取运行目录() + "/.env"))
+        secret = 取环境变量("dingding_secret")
+        webhook = 取环境变量("dingding_webhook")
+        print(secret, webhook)
+
+        机器人 = 钉钉机器人(webhook, secret)
+        # 机器人.发送文本消息("测试")
+        # 机器人.发送链接消息("测试", "测试", "https://www.baidu.com", "https://www.baidu.com/img/bd_logo1.png")
+        # 机器人.发送markdown消息("# 测试\n## 测试\n### 测试\n#### 测试\n#####")
+        # 机器人.发送图片消息("https://www.baidu.com/img/bd_logo1.png")
+
+        机器人.整体跳转消息类型("测试", "![选择](https://www.baidu.com/img/bd_logo1.png) \n### 故事是这样子的...", "https://www.baidu.com")

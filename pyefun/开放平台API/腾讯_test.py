ffd12b0be5e6b5deb2d2eace73495ba6d00cd585
“1715109585” <1715109585@qq.com>
2023-02-20 23:13:02 +0800

    腾讯翻译

---
+++ pyefun/开放平台API/腾讯_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pyefun import *
+from pyefun.调试 import *
+from pyefun.开放平台API.腾讯 import *
+
+
+class Test腾讯(unittest.TestCase):
+
+    def test_阿里云(self):
+        环境变量_从文本中加载至系统(读入文本(取运行目录() + "/.env"))
+
+        tx_secretId = 取环境变量("tx_secretId")
+        tx_secretKey = 取环境变量("tx_secretKey")
+
+        ic(tx_secretId)
+        ic(tx_secretKey)
+
+        翻译结果 = 腾讯翻译(tx_secretId, tx_secretKey, "hello world", "auto", "zh")
+        ic(翻译结果)
+

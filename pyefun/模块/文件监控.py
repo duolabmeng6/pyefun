f17eafcbb74111ad9ae0422b3104714ad93e71d0
“1715109585” <1715109585@qq.com>
2023-02-27 12:08:08 +0800

    文件/目录监控变化

---
+++ pyefun/模块/文件监控.py
@@ -0,0 +1,51 @@
+"""
+需要安装
+pip install watchdog
+"""
+import time
+
+from watchdog.events import *
+from watchdog.observers import Observer
+
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        FileSystemEventHandler.__init__(self)
+
+    def on_moved(self, event):
+        if event.is_directory:
+            print("directory moved from {0} to {1}".format(event.src_path, event.dest_path))
+        else:
+            print("file moved from {0} to {1}".format(event.src_path, event.dest_path))
+
+    def on_created(self, event):
+        if event.is_directory:
+            print("directory created:{0}".format(event.src_path))
+        else:
+            print("file created:{0}".format(event.src_path))
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            print("directory deleted:{0}".format(event.src_path))
+        else:
+            print("file deleted:{0}".format(event.src_path))
+
+    def on_modified(self, event):
+        if event.is_directory:
+            print("directory modified:{0}".format(event.src_path))
+        else:
+            print("file modified:{0}".format(event.src_path))
+
+
+if __name__ == "__main__":
+    observer = Observer()
+    event_handler = FileEventHandler()
+    observer.schedule(event_handler, "C:/test", True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+

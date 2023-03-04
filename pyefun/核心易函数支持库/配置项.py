0bd0dab2a83e887452530d265283dca233e7d535
“1715109585” <1715109585@qq.com>
2023-03-04 12:31:42 +0800

    加入实用功能

--- pyefun/核心易函数支持库/配置项.py
+++ pyefun/核心易函数支持库/配置项.py
@@ -13,8 +13,12 @@
 
 import configparser
 import io
+from pyefun import *
+
 
 class 配置项(configparser.ConfigParser):
+    文件路径 = ""
+
     def __init__(self, 内容=""):
         pass
         super().__init__()
@@ -24,6 +28,15 @@ class 配置项(configparser.ConfigParser):
     def 加载(self, 内容):
         self.read_string(内容)  # python3
         return self
+
+    def 加载从文件(self, 文件路径):
+        self.文件路径 = 文件路径
+        self.read_string(读入文本(文件路径))  # python3
+        return self
+
+    def 保存(self):
+        if self.文件路径:
+            文件_保存(self.文件路径, 到字节集(self.取数据()))
 
     def 取所有项名(self, 节名):
         return self.options(节名)
@@ -62,7 +75,13 @@ class 配置项(configparser.ConfigParser):
         字典 = dict()
         所有节名 = self.取所有节名()
         for 节名 in 所有节名:
-            字典[节名]  = dict()
+            字典[节名] = dict()
             for v in self.取所有项名和值(节名):
                 字典[节名][v[0]] = v[1]
         return 字典
+
+    def 读配置项逻辑值(self, 节名称, 配置项名称, 默认值=False):
+        try:
+            return self.get(节名称, 配置项名称) == "1"
+        except:
+            return 默认值
